36d8694e3771e8f515eb3bd4f71c6c2ab486dd52
Rajesh <das.rajeshkumar2022@gmail.com>
2023-12-25 17:31:12 +0530

    Service changes

--- src/main/java/com/ashokit/service/Pwdutils.java
+++ src/main/java/com/ashokit/service/Pwdutils.java
@@ -6,6 +6,7 @@ import java.util.Base64.Encoder;
 public class Pwdutils {
 
 	public String encodePwd(String pwd) {
+System.out.println("Hellow Rajesh");
 		Encoder encoder = Base64.getEncoder();
 		byte[] encode = encoder.encode(pwd.getBytes());
 		String encodePwd = new String(encode);
